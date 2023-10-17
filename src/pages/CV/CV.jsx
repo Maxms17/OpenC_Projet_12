9501e33bcc48be4f11fdb4963bd93083836301fc
Maxms17 <61410393+Maxms17@users.noreply.github.com>
2023-10-17 11:43:56 +0200

    Modif debut CV

--- src/pages/CV/CV.jsx
+++ src/pages/CV/CV.jsx
@@ -1,12 +1,13 @@
 import React from "react"
 import Layout from "../../components/Layout"
+import Curriculum from "../../components/Curriculum/Curriculum"
 
 import './CV.css'
 
 function CV() {
     return (
         <Layout>
-
+            <Curriculum />
         </Layout>
     )
 }
