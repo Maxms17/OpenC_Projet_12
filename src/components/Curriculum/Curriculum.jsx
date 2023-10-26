47cb52f586dc90af30f3bd149d6140115062fc55
Maxms17 <61410393+Maxms17@users.noreply.github.com>
2023-10-26 16:00:19 +0200

    modif CV font size

--- src/components/Curriculum/Curriculum.jsx
+++ src/components/Curriculum/Curriculum.jsx
@@ -67,57 +67,81 @@ function Curriculum() {
                         </div>
                     </a>  
                 </div>
+                <h2>Langues</h2>
+                <div>
+                    <p>Français - Langue natale</p>
+                    <p>Anglais - Compétences professionnelles limitées</p>
+                </div>
                 <h2>Formation</h2>
                 <div>
                     <h3>Openclassrooms</h3>
                     <h4>INTÉGRATEUR WEB, LE MANS, FÉVRIER 2023 - NOVEMBRE 2023</h4>
-                    <p>    - Réalisation de site internet avec HTML </p>
-                    <p>    - Réalisation de site internet avec CSS </p>
-                    <p>    - Réalisation de site internet avec JavaScript </p>
-                    <p>    - Réalisation de site internet avec React </p>
-                    <p>    - Planification de développement d'un site </p>
-                    <p>    - Optimisation des performances et du référencement </p>
-                    <p>    - Débogage de site internet </p>
-                    <p>    - Travailler en autonomie </p>
+                    <div className='liste'>
+                        <div className='div-liste'>
+                            <p>    - Réalisation de site internet avec HTML </p>
+                            <p>    - Réalisation de site internet avec CSS </p>
+                            <p>    - Réalisation de site internet avec JavaScript </p>
+                            <p>    - Réalisation de site internet avec React </p>
+                        </div>
+                        <div className='div-liste'>
+                            <p>    - Planification de développement d'un site </p>
+                            <p>    - Optimisation des performances et du référencement </p>
+                            <p>    - Débogage de site internet </p>
+                            <p>    - Travailler en autonomie </p>
+                        </div>
+                    </div>
                 </div>
                 <h2>Expérience Professionnelles</h2>
                 <div>
                     <h3>Agent SONO, Vendeur conseil et Agent d'escale</h3>
                     <h4>SNCF - LE MANS ET ANGERS, ÉTÉ 2022 - ÉTÉ 2021 - ÉTÉ 2020</h4>
-                    <p>    - Assurer l'affichage des arrivés et départs en gare </p>
-                    <p>    - Alerter les voyageurs en cas de perturbation </p>
-                    <p>    - Respecter des procédures de sécurité </p>
-                    <p>    - Réaliser des annonces par haut-parleur </p>
-                    <p>    - Assurer la communication entre les services </p>
-                    <p>    - Accueillir une clientèle </p>
-                    <p>    - Étudier une demande client </p>
-                    <p>    - Conseiller un client </p>
-                    <p>    - Assurer le service après-vente </p>
-                    <p>    - Classer des documents professionnels </p>
-                    <p>    - Accueillir les passagers et les informer sur les conditions de voyage </p>
-                    <p>   - Vérifier les titres de transport </p>
-                    <p>   - Renseigner les voyageurs </p>
-                    <p>   - Aider les voyageurs pour les achats sur les bornes en libre service </p>
-                    <p>   - Réaliser les opérations d'embarquement/débarquement, notamment avec le service 
-                            Junior&Cie </p>
-                    <p>    - Veiller à la sécurité des voyageurs </p>
-                    <p>    - Réaliser une procédure de départ sur quai </p>
-                    <p>    - Travailler en équipe </p>
+                    <div className='liste'>
+                        <div className='div-liste'>
+                            <p>    - Assurer l'affichage des arrivés et départs en gare </p>
+                            <p>    - Alerter les voyageurs en cas de perturbation </p>
+                            <p>    - Respecter des procédures de sécurité </p>
+                            <p>    - Réaliser des annonces par haut-parleur </p>
+                            <p>    - Assurer la communication entre les services </p>
+                            <p>    - Accueillir une clientèle </p>
+                            <p>    - Étudier une demande client </p>
+                            <p>    - Conseiller un client </p>
+                            <p>    - Assurer le service après-vente </p>
+                        </div>
+                        <div className='div-liste'>
+                            <p>    - Classer des documents professionnels </p>
+                            <p>    - Accueillir les passagers et les informer sur les conditions de voyage </p>
+                            <p>   - Vérifier les titres de transport </p>
+                            <p>   - Renseigner les voyageurs </p>
+                            <p>   - Aider les voyageurs pour les achats sur les bornes en libre service </p>
+                            <p>   - Réaliser les opérations d'embarquement/débarquement, notamment avec le service 
+                                    Junior&Cie </p>
+                            <p>    - Veiller à la sécurité des voyageurs </p>
+                            <p>    - Réaliser une procédure de départ sur quai </p>
+                            <p>    - Travailler en équipe </p>
+                        </div>
+                    </div>
                 </div>
                 <h2>Parcours Académique</h2>
-                <div>
-                    <h3>Licence informatique</h3>
-                    <p>UNIVERSITÉ DU MANS, LE MANS, 2020-2023</p>
+                <div className='liste'>
+                    <div className='div-liste'>
+                        <h3>Licence informatique</h3>
+                    </div> 
+                    <div className='div-liste'>
+                        <p>UNIVERSITÉ DU MANS, LE MANS, 2020-2023</p>
+                    </div>
                 </div>
-                <div>
-                    <h3>Baccalauréat Scientifique</h3>
-                    <p>LYCÉE NOTRE-DAME, LE MANS, 2017-2020 OPTION ISN</p>
+                <div className='liste'>
+                    <div className='div-liste'>
+                         <h3>Baccalauréat Scientifique</h3>
+                    </div> 
+                    <div className='div-liste'>
+                        <p>LYCÉE NOTRE-DAME, LE MANS, 2017-2020 OPTION ISN</p>
+                    </div>
                 </div>
                 <h2>Compétences</h2>
                 <div>
-                    <h3>Je suis intégrateur web, developpeur front-end</h3>
                     <div className='compétence-col'>
-                        <div className='colonne'>
+                        <div className='colonne-end'>
                             <h4> Front-End </h4>
                             <p>    HTML 5</p>
                             <p>    CSS 3</p>
@@ -125,7 +149,7 @@ function Curriculum() {
                             <p>    Langage C</p>
                             <p>    Python</p>
                         </div>
-                        <div className='colonne'>
+                        <div className='colonne-end'>
                             <h4> Back-End </h4>
                             <p>    Node.Js</p>
                             <p>    MongoDB</p>
@@ -149,11 +173,6 @@ function Curriculum() {
                             <p>    Concevoir et réaliser un proje</p>
                         </div>
                     </div>
-                </div>
-                <h2>Langues</h2>
-                <div>
-                    <p>Français - Langue natale</p>
-                    <p>Anglais - Compétences professionnelles limitées</p>
                 </div>
             </div>
         </div>
